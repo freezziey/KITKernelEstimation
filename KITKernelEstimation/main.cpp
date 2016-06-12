e9428d38378fc890d975d5e90c1506b8da6c4696
Christian Fries <h.t.user@web.de>
2016-06-12 13:23:16 +0200

    Initial Commit

---
+++ KITKernelEstimation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KITKernelEstimation
+//
+//  Created by Christian Fries on 12.06.16.
+//  Copyright © 2016 Christian Fries. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
